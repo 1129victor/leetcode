c9d19ee453a50f4bf668c2894c4c8eda7e1c0df3
1129victor <bmw61102@gmail.com>
2020-06-02 16:47:28 +0800

    1281. Subtract the Product and ...
    1281. Subtract the Product and Sum of Digits of an Integer
    Difficulty:Easy

---
+++ 1281.py
@@ -0,0 +1,44 @@
+from typing import List
+
+#from collections import Counter
+
+class inputValue:
+    def inputValueValidation(self) -> List[int]:
+          while True:
+            try:
+                #arr = list(map(int, input("Please input an array (no input to quit) : ").split()))
+                num1 = int(input("Please input an integer (-1 to quit) : "))
+            except ValueError:
+                print("Not a string! Try again.")  
+                continue
+            else:
+                return num1
+                break
+            
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        product = 1
+        sum1 = 0
+        for c in str(n):
+            product *= int(c)
+            sum1 += int(c)
+        return product - sum1
+            
+       
+        
+while True:
+    
+    UserInput = inputValue()
+    
+    num1 = UserInput.inputValueValidation();
+    
+    
+    if num1 == -1:
+        break
+    
+    
+    ansclass = Solution()
+    
+    result = ansclass.subtractProductAndSum(num1)
+    
+    print(result)
