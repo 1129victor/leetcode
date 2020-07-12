464df8f46ea58be46224262ee11472a1f5ba3be6
Victor.lin <bmw61102@gmail.com>
2020-07-12 17:04:56 +0800

    965. Univalued Binary Tree

---
+++ 965.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isUnivalTree(self, root):
+        if not root:
+            return True
+        
+        if root.right:
+            if root.val != root.right.val:
+                return False
+            
+        if root.left:
+            if root.val != root.left.val:
+                return False
+        
+        return self.isUnivalTree(root.right) and self.isUnivalTree(root.left)
+    
+    
+    '''
+        queue = []
+        queue.append(root)
+        val = root.val
+        while queue:
+            temp = queue.pop()
+            if temp.val != val:
+                return False
+            if temp.left:
+                queue.append(temp.left)
+            if temp.right:
+                queue.append(temp.right)
+        return True
+    '''
